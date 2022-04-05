31f2d61ee00052b1e55d080ca0289620bb5ac1a7
MIN <keimindev@gmail.com>
2022-04-06 01:06:13 +0900

    03. add menu icon , drawer

--- lib/main.dart
+++ lib/main.dart
@@ -9,140 +9,84 @@ void main() => runApp(MyApp());
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-     //반드시 arguement를 가져야함
+    //반드시 arguement를 가져야함
     return MaterialApp(
-    debugShowCheckedModeBanner: false,
-     title: "BBANTO",
+      debugShowCheckedModeBanner: false,
+      title: "Snack Bar",
       // 정상적으로 실행되었을 때 제일 먼저 보여주는 곳이 home
-      home: Grade(),
+      home: MyPage(),
     );
   }
 }
 
-class Grade extends StatelessWidget {
+class MyPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     // web 화면구성을 위해 도와주는 도화지같은 요소 이거 필수임
     return Scaffold(
-      backgroundColor: Colors.amber[800],
+      backgroundColor: Colors.red,
       appBar: AppBar(
-        title: Text('BBANTO'),
+        title: Text('Snack Bar'),
         centerTitle: true,
-        backgroundColor: Colors.amber[700],
         elevation: 0.0,
       ),
-      //padding은 세로축상으로는 제약이 없다. 이건 세로축 중앙
-      //가로축 정중앙으로 보내려면 center를 쓴다
-      body:  Padding(
-        padding: EdgeInsets.fromLTRB(30.0, 40.0, 0.0, 0.0),
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
+      drawer: Drawer(
+        child: ListView(
+          padding: EdgeInsets.zero,
           children: <Widget>[
-            Center(
-              child: CircleAvatar(
-                backgroundImage: AssetImage('assets/reactjs.png'),
-                radius: 60.0,
+            UserAccountsDrawerHeader(
+              currentAccountPicture: CircleAvatar(
+                backgroundImage: AssetImage('assets/moon.png'),
+                backgroundColor: Colors.white,
               ),
+              otherAccountsPictures: <Widget>[
+                CircleAvatar(
+                  backgroundImage: AssetImage('assets/redux.png'),
+                  backgroundColor: Colors.white,
+                ),
+              ],
+              accountName: Text('Moon'),
+              accountEmail: Text('moon@gmail.com'),
+              onDetailsPressed: () {
+                print('open drawer');
+              },
+              decoration: BoxDecoration(
+                  color: Colors.red[200],
+                  borderRadius: BorderRadius.only(
+                    bottomLeft: Radius.circular(30.0),
+                    bottomRight: Radius.circular(30.0),
+                  )),
             ),
-            Divider(
-              height: 60.0,
-              color: Colors.grey[850],
-              thickness: 0.5,
-              endIndent: 30.0,
+            ListTile(
+              leading: Icon(Icons.home, color: Colors.grey[850]),
+              title: Text("Home"),
+              //gesture
+              onTap: () {
+                print('Home is clicked');
+              },
+              trailing: Icon(Icons.add),
             ),
-            Text('Name',
-            style: TextStyle(
-              color: Colors.white,
-              letterSpacing: 2.0,
-            )
+            ListTile(
+              leading: Icon(Icons.settings, color: Colors.grey[850]),
+              title: Text("Setting"),
+              //gesture
+              onTap: () {
+                print('Setting is clicked');
+              },
+              trailing: Icon(Icons.add),
             ),
-            //height , width 간격을 만들수 있게 해준다. 높이를 주면
-            SizedBox(
-              height: 10.0,
+            ListTile(
+              leading: Icon(Icons.question_answer, color: Colors.grey[850]),
+              title: Text("Q&A"),
+              //gesture
+              onTap: () {
+                print('Q&A is clicked');
+              },
+              trailing: Icon(Icons.add),
             ),
-             Text('BBANTO', 
-             style: TextStyle(
-               color: Colors.white,
-               letterSpacing: 2.0,
-               fontSize: 28.0,
-               fontWeight: FontWeight.bold,
-               ),
-               ),
-                  SizedBox(
-              height: 30.0,
-            ),
-            Text('BBANTO POWER LEVEL',
-            style: TextStyle(
-              color: Colors.white,
-              letterSpacing: 2.0,
-            )
-            ),//height , width 간격을 만들수 있게 해준다. 높이를 주면
-            SizedBox(
-              height: 10.0,
-            ),
-             Text('14', 
-             style: TextStyle(
-               color: Colors.white,
-               letterSpacing: 2.0,
-               fontSize: 28.0,
-               fontWeight: FontWeight.bold,
-               ),
-               ),
-              SizedBox(
-              height: 30.0,
-              ),
-               Row(
-                 children: <Widget>[
-                   Icon(Icons.check_circle_outline),
-                   SizedBox(
-                     width: 10.0,
-                   ),
-                   Text('using lightsaber', 
-                   style: TextStyle(
-                     fontSize: 16.0,
-                     letterSpacing: 1.0,
-
-                   ),),
-                 ],
-               ),
-                  Row(
-                 children: <Widget>[
-                   Icon(Icons.check_circle_outline),
-                   SizedBox(
-                     width: 10.0,
-                   ),
-                   Text('fase hero tattoo', 
-                   style: TextStyle(
-                     fontSize: 16.0,
-                     letterSpacing: 1.0,
-
-                   ),),
-                 ],
-               ),
-                              Row(
-                 children: <Widget>[
-                   Icon(Icons.check_circle_outline),
-                   SizedBox(
-                     width: 10.0,
-                   ),
-                   Text('fire flames', 
-                   style: TextStyle(
-                     fontSize: 16.0,
-                     letterSpacing: 1.0,
-
-                   ),),
-                 ],
-               ),
-               Center(
-                 child: CircleAvatar(
-                   backgroundImage: AssetImage('assets/redux.png'),
-                   radius: 40.0,
-                   backgroundColor: Colors.amber[800],
-                   ),
-                   ),
           ],
-        )
-      )
+        ),
+      ),
     );
   }
 }
